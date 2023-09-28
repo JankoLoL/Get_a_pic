7398926dce81e20d63309497652df88a17079853
JankoLoL <adam.chch86@gmail.com>
2023-09-28 11:44:38 +0200

    Models created

--- get_a_pic_django/get_a_pic_app/models.py
+++ get_a_pic_django/get_a_pic_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ThumbnailSize(models.Model):
+    size = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return f"{self.size}px"
+
+
+class Plan(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    thumbnail_sizes = models.ManyToManyField(ThumbnailSize, blank=True)
+    has_original_image_link = models.BooleanField(default=False)
+    can_generate_expiring_link = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Image(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="images")
+    image_file = models.ImageField(upload_to="uploaded_images/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image {self.id} by {self.user.username}"
