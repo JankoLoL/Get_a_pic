c195dc879b5e55028f9fca15421136794a91ba24
JankoLoL <adam.chch86@gmail.com>
2023-09-28 10:55:07 +0200

    database and admin

--- get_a_pic_django/get_a_pic_django/settings.py
+++ get_a_pic_django/get_a_pic_django/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'get_a_pic_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'get_a_pic_db',
+        'USER': 'postgres',
+        'PASSWORD': 'coderslab',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
