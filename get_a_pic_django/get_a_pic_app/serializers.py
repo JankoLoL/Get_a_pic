147ddea5217a08df65f7e89d9a0f098158e9581c
JankoLoL <adam.chch86@gmail.com>
2023-09-30 12:38:34 +0200

    userprofile, images, plans views

--- get_a_pic_django/get_a_pic_app/serializers.py
+++ get_a_pic_django/get_a_pic_app/serializers.py
@@ -9,23 +9,37 @@ class ThumbnailSizeSerializer(serializers.ModelSerializer):
 
 
 class PlanSerializer(serializers.ModelSerializer):
-    thumbnail_size = ThumbnailSizeSerializer(many=True)
+    thumbnail_sizes = ThumbnailSizeSerializer(many=True)
 
     class Meta:
         model = Plan
-        fields = ('id', 'name', 'thumbnail_size', 'has_original_image_link', 'can_generate_expiring_link')
+        fields = ('id', 'name', 'thumbnail_sizes', 'has_original_image_link', 'can_generate_expiring_link')
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    thumbnail_200 = serializers.SerializerMethodField()
+    thumbnail_400 = serializers.SerializerMethodField()
+
     class Meta:
         model = Image
-        fields = ('id', 'user', 'image_file', 'uploaded_at')
+        fields = ('id', 'user', 'image_file', 'uploaded_at', 'thumbnail_200', 'thumbnail_400')
+
+    def _get_thumbnail_url(self, obj, size):
+        file_extension = obj.image_file.name.split(".")[-1].lower()
+        return f"/media/thumbnails/{obj.id}_{size}.{file_extension}"
+
+    def get_thumbnail_200(self, obj):
+        return self._get_thumbnail_url(obj, 200)
+
+    def get_thumbnail_400(self, obj):
+        return self._get_thumbnail_url(obj, 400)
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
     plan = PlanSerializer()
+    images = ImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = UserProfile
-        fields = ('user', 'plan')
+        fields = ('user', 'plan', 'images')
--- get_a_pic_django/get_a_pic_app/urls.py
+++ get_a_pic_django/get_a_pic_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet, ImageViewSet, MainPageView
+from .views import UserProfileViewSet, ImageViewSet, MainPageView, PlanViewSet, ThumbnailSizeViewSet
 from django.contrib.auth import views as auth_views
 
 router = DefaultRouter()
 router.register(r'userprofile', UserProfileViewSet)
 router.register(r'images', ImageViewSet)
+router.register(r'plans', PlanViewSet)
+router.register(r'thumbnail-sizes', ThumbnailSizeViewSet)
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main-page'),
