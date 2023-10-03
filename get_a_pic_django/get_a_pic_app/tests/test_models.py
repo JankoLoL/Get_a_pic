27d3e1b0e021c756b526e1f8dce6e97093178dfb
JankoLoL <adam.chch86@gmail.com>
2023-10-03 13:53:43 +0200

    Tests 2 for Image model

--- get_a_pic_django/get_a_pic_app/tests/test_models.py
+++ get_a_pic_django/get_a_pic_app/tests/test_models.py
@@ -1,25 +1,39 @@
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
-from ..models import Image, User
+from ..models import Image
+from django.contrib.auth.models import User
 import os
 from django.db import IntegrityError
+from django.conf import settings
 
 
 class ImageModelTestCase(TestCase):
-    def test_image_creation(self):
-        user = User.objects.create_user(username='testuser', password='testpassword')
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        image_path = os.path.join(current_dir, 'test_image.jpg')
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.image_path = os.path.join(self.current_dir, 'test_image.jpg')
 
-        with open(image_path, 'rb') as file:
+        with open(self.image_path, 'rb') as file:
             image_content = file.read()
 
         uploaded_image = SimpleUploadedFile(name='test_image.jpg', content=image_content, content_type='image/jpeg')
+        self.image = Image.objects.create(image_file=uploaded_image, user=self.user)
 
-        image = Image.objects.create(image_file=uploaded_image, user=user)
+    def test_image_creation(self):
+        self.assertIsInstance(self.image, Image)
+        self.assertIn('test_image', self.image.image_file.name)
 
-        self.assertIsInstance(image, Image)
-        self.assertIn('test_image', image.image_file.name)
+    def test_image_string_representation(self):
+        self.assertEqual(str(self.image), f"Image {self.image.id} by {self.user.username}")
+
+    def test_create_thumbnail(self):
+        thumbnail_path = self.image.create_thumbnail(size=200)
+        expected_path_format = f"thumbnails/{self.image.id}_200.jpg"
+        self.assertEqual(thumbnail_path, expected_path_format)
+        self.assertTrue(os.path.exists(os.path.join(settings.MEDIA_ROOT, thumbnail_path)))
+
+    def test_image_belongs_to_user(self):
+        self.assertEqual(self.image.user, self.user)
 
 
 class UserModelTestCase(TestCase):
